34e0334ecea211a93b14085c56fda3976fa07307
J-DouglasLira <jdouglas.developer@gmail.com>
2022-06-06 11:25:37 -0300

    style: main menu and main-nav

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import "./styles.css";
 export function NavBar() {
   return (
-    <nav className="navbar navbar-expand-md bg-primary">
+    <nav className="navbar navbar-expand-md bg-primary main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-logo-text">
           <h4>DS Catalog</h4>
@@ -9,7 +9,7 @@ export function NavBar() {
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link">HOME</a>
+              <a href="link" className="active">HOME</a>
             </li>
             <li>
               <a href="link">CATÁLOGO</a>
