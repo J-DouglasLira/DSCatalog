6ccb718739e7f824a9680e5d1449bdfcaab7198b
J-DouglasLira <jdouglas.developer@gmail.com>
2022-06-09 10:33:28 -0300

    feat: buttonIcon add in Homepage

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { NavBar } from "components/Navbar";
 import "./styles.css";
 import { ReactComponent as MainImage} from "assets/images/main-image.svg";
+import ButtonIcon from "components/ButtonIcon";
 
 const Home = () => {
   return (
@@ -10,6 +11,12 @@ const Home = () => {
       <div className="home-card">
         <div className="home-content-container">
           <h1>Discover the best product catalog</h1>
+          <p>
+            We will help you find the
+            best products available
+            on the market.
+          </p>
+          <ButtonIcon/>
         </div>
         <div className="home-image-container">
           <MainImage />
