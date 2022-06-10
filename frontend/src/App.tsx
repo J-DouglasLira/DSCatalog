7f4aa5a15f6fca79156e16e2815860d432ea4d5a
J-DouglasLira <jdouglas.developer@gmail.com>
2022-06-10 09:44:31 -0300

    feat: catalog component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import "./assets/styles/custom.scss";
 import "./App.css";
 import Home from "pages/Home";
+import Catalog from "pages/Catalog";
 
 function App() {
   return (
   <>
     <Home />
-  </>);
+    <Catalog />
+  </>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Catalog/index.tsx
@@ -0,0 +1,8 @@
+const Catalog = () => {
+  return (
+    <div className="container my-4">
+      <h1>Tela Catalog</h1>
+    </div>
+  );
+};
+export default Catalog;
