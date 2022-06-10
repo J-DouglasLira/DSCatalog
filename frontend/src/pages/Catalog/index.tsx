464d9bce2c94106873269df88957b0945caa3683
J-DouglasLira <jdouglas.developer@gmail.com>
2022-06-10 10:02:13 -0300

    feat: product card caled into catalog

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,7 +1,9 @@
+import ProductCard from "components/ProductCard";
+
 const Catalog = () => {
   return (
     <div className="container my-4">
-      <h1>Tela Catalog</h1>
+      <ProductCard />
     </div>
   );
 };
