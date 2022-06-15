cae3770e088363d6384356fbc19baa23673196d5
J-DouglasLira <jdouglas.developer@gmail.com>
2022-06-15 10:48:16 -0300

    fix: remove duplicate item

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,4 @@
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { NavLink,Link  } from "react-router-dom";
 import "./styles.css";
 export function NavBar() {
   return (
