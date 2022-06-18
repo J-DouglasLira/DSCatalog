89033986096ff1b01234bc92ea6c7c5c4b54dda6
J-DouglasLira <jdouglas.developer@gmail.com>
2022-06-18 14:40:10 -0300

    feat: pagination items

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,17 @@
+import { ReactComponent as ArrowIcon } from "assets/images/arrow.svg";
+
+const Pagination = () => {
+  return (
+    <div className="pagination-container">
+      <ArrowIcon />
+      <div className="pagination-items">1</div>
+      <div className="pagination-items">2</div>
+      <div className="pagination-items">3</div>
+      <div className="pagination-items">...</div>
+      <div className="pagination-items">10</div>
+      <ArrowIcon />
+    </div>
+  );
+};
+
+export default Pagination;
