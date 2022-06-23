13f98c73266675dc12cb31070d60fe42502b97f8
J-DouglasLira <jdouglas.developer@gmail.com>
2022-06-23 19:32:52 -0300

    feat: admin routes

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,12 +1,30 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import "./styles.css";
+
+const Component1 = () => {
+  return <h1>Products 1</h1>;
+};
+
+const Component2 = () => {
+  return <h1>Categories </h1>;
+};
+
+const Component3 = () => {
+  return <h1>Users 2</h1>;
+};
 
 const Admin = () => {
   return (
     <div className="admin-container">
       <Navbar />
       <div className="admin-content">
-        <h1>Conteudo</h1>
+        <Routes>
+          <Route path="products" element={<Component1 />} />
+          <Route path="categories" element={<Component2 />} />
+          <Route path="users" element={<Component3 />} />
+        </Routes>
+        <div />
       </div>
     </div>
   );
