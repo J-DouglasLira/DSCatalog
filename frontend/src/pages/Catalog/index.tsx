e730fdbcf99f4423e785d3120e52a66de6154e5d
J-DouglasLira <jdouglas.developer@gmail.com>
2022-06-29 11:14:47 -0300

    style: add cardLoader component

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -7,41 +7,50 @@ import { Product } from "types/product";
 import { AxiosParams } from "types/vendor/axios";
 import { SpringPage } from "types/vendor/spring";
 import { BASE_URL } from "util/requests";
+import CardLoader from "./CardLoader";
 import "./styles.css";
 
 const Catalog = () => {
+
   const [page, setPage] = useState<SpringPage<Product>>();
+  const [isLoading, setIsLoading] = useState<boolean>();
+
 
   useEffect(() => {
-    const params: AxiosParams = {
-      method: "GET",
-      url: `${BASE_URL}/products`,
-      params: {
-        page: 0,
-        size: 12
-      }
-    };
-    axios(params).then(response => {
-      setPage(response.data);
-    });
+    loadPage();
   }, []);
 
+function loadPage() {
+  const params: AxiosParams = {
+    method: "GET",
+    url: `${BASE_URL}/products`,
+    params: {
+      page: 0,
+      size: 12
+    }
+  };
+  setIsLoading(true);
+  axios(params).then(response => {
+    setPage(response.data);
+  }).finally(()=>{
+    setIsLoading(false);
+  });
+  }
 
 
   return (
     <div className="container my-4 catalog-container">
       <div className="row catalog-title-container">
-        <h1>Catálogo de Produtos</h1>
+        <h1>Products Catalog</h1>
       </div>
       <div className="row">
 
-        {page?.content.map((product)=>(
+        {isLoading ? <CardLoader/>:  (page?.content.map((product)=>(
             <div className="col-sm-6 col-lg-4 col-xl-3" key={product.id}>
               <Link to={"/products/" + product.id}>
                 <ProductCard product={product} />
               </Link>
-            </div>
-        ))}
+            </div>)))}
 
         
       </div>
