67bb9912f2e1585424b75147a05547c47d56f129
J-DouglasLira <jdouglas.developer@gmail.com>
2022-07-20 10:22:19 -0300

    feat: requestBackendLogin

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -1,9 +1,11 @@
+import axios from 'axios';
+import qs from 'qs';
+
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
-
 
 type LoginData = {
   username: string;
@@ -12,7 +14,18 @@ type LoginData = {
 export const requestBackendLogin = (loginData: LoginData) => {
   const headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Basic' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
-  }
+    Authorization: 'Basic' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+  };
+  const data = qs.stringify({
+    ...loginData,
+    grand_type: 'password',
+  });
 
+  return axios({
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers,
+  });
 };
